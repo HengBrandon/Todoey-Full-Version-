2e1d445c1ea63ac40ca6600f2ab77bdb34422031
Heng Brandon <heng.brandon@yahoo.com>
2018-08-29 11:00:23 +0700

    Add new item functionality Complete.

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -10,7 +10,8 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
-    let itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorogon"]
+    var itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorogon"]
+        //@IBOutlet var myTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +23,7 @@ class TodoListViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
     
+
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         //let cell = UITableViewCell(style: default, reuseIdentifier: "ToDoItemCell")
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
@@ -44,6 +46,24 @@ class TodoListViewController: UITableViewController {
         }
         tableView.deselectRow(at: indexPath, animated: true)
     }
-
+    
+    //MARK - Add New Items
+    @IBAction func addButtonPress(_ sender: UIBarButtonItem) {
+        var textfiled = UITextField()
+        let alert = UIAlertController(title: "Add New Todoey It", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Item", style: .default){ (action) in
+            //what happen when user click add item button
+            self.itemArray.append(textfiled.text!)
+            print("Hello \(String(describing: textfiled.text))")
+            self.tableView.reloadData()
+        }
+        alert.addTextField { (alterTextFile) in
+            alterTextFile.placeholder = "Create new item"
+            textfiled = alterTextFile
+        }
+        alert.addAction(action)
+        present(alert,animated: true, completion: nil)
+    }
+    
 }
 
